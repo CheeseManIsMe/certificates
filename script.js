3a9d32f9285fe92371fc82bd80ddca82abca7aaf
CheeseManIsMe <aiden.beijens24@student.cart.org>
2025-02-06 21:48:26 -0800

    lots

---
+++ script.js
@@ -0,0 +1,37 @@
+function certGoblin(){
+    const element = document.getElementById(`certificate`);
+    element.classList.remove(`certificateW`, `certificateR`);
+    element.classList.add(`certificateG`);
+}
+
+function certGamble(){
+    const element = document.getElementById(`certificate`);
+    element.classList.remove(`certificateW`, `certificateG`);
+    element.classList.add(`certificateR`);
+}
+
+function certWorst() {
+    const element = document.getElementById(`certificate`);
+    element.classList.remove(`certificateG`, `certificateR`);
+    element.classList.add(`certificateW`);
+}
+
+function printCert() {
+    let openWindow=window.open(``, `title`, `attributes`);
+    openWindow.document.write
+}
+
+function recipientName() {
+    let recipient = document.getElementById(`rNameInput`);
+    document.getElementById(`recipientNameTxt`).innerHTML = recipient.value;
+}
+
+function awarderName() {
+    let awarder = document.getElementById(`aNameInput`);
+    document.getElementById(`awarderNameTxt`).innerHTML = awarder.value;
+}
+
+function dateChange() {
+    let date = document.getElementById(`dateInput`);
+    document.getElementById(`dateTxt`).innerHTML = date.value;
+}
